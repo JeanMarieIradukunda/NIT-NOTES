42d67e8421381ff130828a75cf0be3ce81c8bb35
JeanMarieIradukunda <jeanmarievianneyiradukunda411@gmail.com>
2026-09-22 11:30:58 +0200

    Errors of accounts urls and settings corrected

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, reverse_lazy
 
 from . import forms, views
 
@@ -17,7 +17,8 @@ urlpatterns = [
     path("password-change/",
          auth_views.PasswordChangeView.as_view(
              template_name="accounts/password_change.html",
-             form_class=forms.StyledPasswordChangeForm),
+             form_class=forms.StyledPasswordChangeForm,
+             success_url=reverse_lazy("accounts:password_change_done")),
          name="password_change"),
     path("password-change/done/",
          auth_views.PasswordChangeDoneView.as_view(template_name="accounts/password_change_done.html"),
--- nit_platform/settings.py
+++ nit_platform/settings.py
@@ -150,7 +150,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # --------------------------------------------------------------------------- #
 
 LANGUAGE_CODE = "en-us"
-TIME_ZONE = os.environ.get("DJANGO_TIME_ZONE", "UTC")
+TIME_ZONE = (os.environ.get("DJANGO_TIME_ZONE") or "UTC").strip()
 USE_I18N = True
 USE_TZ = True
 
